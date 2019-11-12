2be84e00441c3fd707a27461b54403f9c6a3f390
Raiden <vayren0965@gmail.com>
2019-11-12 19:40:58 +0200

    AnimationForSlider_DotClick

--- index.js
+++ index.js
@@ -31,7 +31,53 @@ function slider(){
 	}
 }
 
-setInterval(slider, 2500);
+function switchSlide(n) {
+	let dots = document.querySelectorAll('.dot');
+	let slides = document.querySelectorAll('.slide');
+	let posts = document.querySelectorAll('.post');
+
+	for(let i = 0;i < dots.length;i++){
+		if(dots[i].classList.contains('active')){
+			dots[i].classList.remove('active');				
+			slides[i].classList.remove('visible');
+			posts[i].classList.remove('visible');
+		}
+	}
+
+	dots[n].classList.add('active');
+	slides[n].classList.add('visible');
+	posts[n].classList.add('visible');
+}
+	
+let time = 4000;
+let timer = setInterval(slider, time);
+
+
+function dotClick() {
+	let dotsBlock = document.querySelector('.dots');
+	dotsBlock.onclick = function(event){
+		let elem = event.target;
+		if(elem.tagName == 'SPAN'){
+			if(elem.nextElementSibling === null){
+				clearInterval(timer);
+				switchSlide(2);
+				timer = setInterval(slider, time);
+			}
+			else if(elem.previousElementSibling === null){
+				clearInterval(timer);
+				switchSlide(0);
+				timer = setInterval(slider, time);
+			} 
+			else{
+				clearInterval(timer);
+				switchSlide(1);
+				timer = setInterval(slider, time);
+			}
+		}
+	}
+}
+
+dotClick();
 
 
 
